f13bb51575a023713b71189785b28a5c77ee5228
AKILENG ISAAC <isaacakileng56@gmail.com>
2017-06-06 17:26:43 +0300

    Update modules

--- typings/rc-slider.d.ts
+++ typings/rc-slider.d.ts
@@ -1,7 +1,47 @@
-import "rc-slider";
-
 declare module "rc-slider" {
-    interface CommonApiProps {
-        customTrackClass?: string;
+    export interface Marks {
+        [number: number]: JSX.Element | string | { style: any, label: string | JSX.Element };
     }
+
+    interface CommonApiProps {
+        className?: string;
+        min?: number;
+        max?: number;
+        marks?: Marks;
+        step?: number;
+        vertical?: boolean;
+        handle?(props: any): React.ReactNode;
+        included?: boolean;
+        disabled?: boolean;
+        dots?: boolean;
+        onBeforeChange?(value: any): any | undefined;
+        onChange?(value: any): any | undefined;
+        onAfterChange?(value: any): any | undefined;
+        tipTransitionName?: string;
+        tipFormatter?: ((value: any) => any | undefined) | null;
+    }
+
+    interface SliderProps extends CommonApiProps {
+        defaultValue?: number;
+        value?: number;
+    }
+
+    interface RangeProps extends CommonApiProps {
+        defaultValue?: number[];
+        value?: number[];
+        count?: number;
+        allowCross?: boolean;
+        pushable?: boolean;
+    }
+
+    interface HandleProps extends CommonApiProps {
+        className: string;
+        vertical: boolean;
+        offset: number;
+    }
+
+    export class Range extends React.Component<RangeProps, {}> { }
+    export class Handle extends React.Component<HandleProps, {}> { }
+    export default class Slider extends React.Component<SliderProps, {}> { }
+
 }
